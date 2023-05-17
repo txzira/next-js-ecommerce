34adc5cb09b93beb395ad37961b946c27069e7ba
skira <storeadminwest@protonmail.com>
2023-05-17 03:49:21 -0700

    Prototype

--- app/api/delete-unverified-accounts/route.ts
+++ app/api/delete-unverified-accounts/route.ts
@@ -1,9 +1,9 @@
 import prisma from "lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function POST() {
-  const users = await prisma.user.findMany({ where: { verifiedEmail: false } });
-  if (users && users.length > 0) {
-    return NextResponse.json({ ok: true });
-  }
+export async function GET() {
+  // const users = await prisma.user.findMany({ where: { verifiedEmail: false } });
+  // if (users && users.length > 0) {
+  return NextResponse.json({ ok: true });
+  // }
 }
