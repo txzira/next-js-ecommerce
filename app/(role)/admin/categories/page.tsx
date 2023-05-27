33556e182a2acb9c637e0c7163778ac27cb6494e
skira <storeadminwest@protonmail.com>
2023-05-27 14:10:11 -0700

    Refactor WalletAddress to CryptoWallet and WalletType to CryptoWalletType 15

--- app/(role)/admin/categories/page.tsx
+++ app/(role)/admin/categories/page.tsx
@@ -1,11 +1,16 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { CategoryList, CategoryCreateForm } from "./Categories";
 import useSWR from "swr";
 
 export default function AdminCategoryPage() {
   const fetcher = async (url) => fetch(url).then((res) => res.json());
   const { data: categoriesData, error, isLoading, mutate: categoriesMutate } = useSWR("/admin/categories/get-categories", fetcher);
+
+  useEffect(() => {
+    categoriesMutate();
+  });
+
   return (
     <div className="h-full w-[90%] mx-auto">
       <h1 className="text-3xl font-bold pb-5">Categories</h1>
