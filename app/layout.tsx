278a8daefb0e89f21c83db86433276c233b0c094
txzira <txziraio@gmail.com>
2024-06-15 02:17:21 -0700

    implement meta data for next js 14

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,21 @@ import NextAuthSessionProvider from "./NextAuthSessionProvider";
 import Header from "./(navigation)/Header";
 import { Toaster } from "react-hot-toast";
 import Background from "./Background";
+import { Metadata, Viewport } from "next";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  // Also supported by less commonly used
+  // interactiveWidget: 'resizes-visual',
+};
+
+export const metadata: Metadata = {
+  title: "Next js ecommerce",
+  description: "Modern ecommerce application using nextjs and prisma",
+  creator: "Ronnie Ayers",
+  generator: "Next.js",
+};
 
 export default function RootLayout({
   children,
@@ -16,8 +31,10 @@ export default function RootLayout({
         <body>
           <Toaster />
           <Header />
-          <Background/>
-          <main className="relative h-[calc(100vh_-_3.5rem)] md:h-[calc(100vh_-_3.5rem)] ">{children}</main>
+          <Background />
+          <main className="relative h-[calc(100vh_-_3.5rem)] md:h-[calc(100vh_-_3.5rem)] ">
+            {children}
+          </main>
         </body>
       </html>
     </NextAuthSessionProvider>
