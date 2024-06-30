86c70b4e242803359899a7d3209a24b16eaa7075
txzira <txziraio@gmail.com>
2024-06-29 23:59:25 -0700

    Prepare project for first successful deployment

--- app/(role)/admin/orders/page.tsx
+++ app/(role)/admin/orders/page.tsx
@@ -1,34 +1,35 @@
-// "use client";
-// import React, { useState } from "react";
+"use client";
+import React, { useState } from "react";
 // import { OrderDetails, OrderTable } from "./Orders";
-// import { OrderSummary } from "types/ordersummary";
-// import useSWR from "swr";
+import { OrderSummary } from "types/ordersummary";
+import useSWR from "swr";
 
-// export default function AdminOrdersPage() {
-//   const [order, setOrder] = useState<OrderSummary>();
-//   const [limit, setLimit] = useState(10);
-//   const [cursor, setCursor] = useState(0);
-//   const [sort, setSort] = useState("asc");
-//   const {
-//     data: ordersData,
-//     error: ordersError,
-//     isLoading: ordersIsLoading,
-//     mutate: ordersMutate,
-//   } = useSWR<{
-//     orders: OrderSummary[];
-//     count: number;
-//   }>(`/admin/orders/get-orders/${limit}/${cursor}/${sort}`, (url) => fetch(url, { method: "GET" }).then((res) => res.json()));
-//   return (
-//     <div className="h-full w-[90%] mx-auto">
-//       <OrderTable
-//         ordersData={ordersData}
-//         setOrder={setOrder}
-//         ordersIsLoading={ordersIsLoading}
-//         setCursor={setCursor}
-//         limit={limit}
-//         setLimit={setLimit}
-//       />
-//       {order ? <OrderDetails order={order} ordersMutate={ordersMutate} setOrder={setOrder} /> : null}
-//     </div>
-//   );
-// }
+export default function AdminOrdersPage() {
+  //   const [order, setOrder] = useState<OrderSummary>();
+  //   const [limit, setLimit] = useState(10);
+  //   const [cursor, setCursor] = useState(0);
+  //   const [sort, setSort] = useState("asc");
+  //   const {
+  //     data: ordersData,
+  //     error: ordersError,
+  //     isLoading: ordersIsLoading,
+  //     mutate: ordersMutate,
+  //   } = useSWR<{
+  //     orders: OrderSummary[];
+  //     count: number;
+  //   }>(`/admin/orders/get-orders/${limit}/${cursor}/${sort}`, (url) => fetch(url, { method: "GET" }).then((res) => res.json()));
+  //   return (
+  //     <div className="h-full w-[90%] mx-auto">
+  //       <OrderTable
+  //         ordersData={ordersData}
+  //         setOrder={setOrder}
+  //         ordersIsLoading={ordersIsLoading}
+  //         setCursor={setCursor}
+  //         limit={limit}
+  //         setLimit={setLimit}
+  //       />
+  //       {order ? <OrderDetails order={order} ordersMutate={ordersMutate} setOrder={setOrder} /> : null}
+  //     </div>
+  //   );
+  return <div></div>;
+}
