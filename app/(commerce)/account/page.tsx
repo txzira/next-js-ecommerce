23ef8ba3fb9a9c175810bc883a7bfaf79d7ba31f
txzira <txziraio@gmail.com>
2024-06-29 23:15:36 -0700

    Prepare project for first successful deployment

--- app/(commerce)/account/page.tsx
+++ app/(commerce)/account/page.tsx
@@ -1,24 +1,24 @@
-// "use client";
+"use client";
 // import { Order, Image, ShippingAddress, Cart, CartItem } from "@prisma/client";
-// import React, { useState } from "react";
+import React, { useState } from "react";
 // import { AccountInformation, AccountHistory, OrderDetails } from "./AccountHistory";
 
-// export default function AccountPage() {
-//   const [order, setOrder] = useState<
-//     Order & {
-//       cart: Cart & {
-//         cartItems: CartItem[];
-//       };
-//       shipping: ShippingAddress;
-//       image: Image;
-//     }
-//   >();
-//   //
-//   return (
-//     <div className="flex flex-col mt-8 ">
-//       <AccountInformation />
-//       <AccountHistory setOrder={setOrder} />
-//       {order ? <OrderDetails order={order} setOrder={setOrder} /> : null}
-//     </div>
-//   );
-// }
+export default function AccountPage() {
+  // const [order, setOrder] = useState<
+  //   Order & {
+  //     cart: Cart & {
+  //       cartItems: CartItem[];
+  //     };
+  //     shipping: ShippingAddress;
+  //     image: Image;
+  //   }
+  // >();
+  //
+  return (
+    <div className="mt-8 flex flex-col ">
+      {/* <AccountInformation />
+      <AccountHistory setOrder={setOrder} />
+      {order ? <OrderDetails order={order} setOrder={setOrder} /> : null} */}
+    </div>
+  );
+}
