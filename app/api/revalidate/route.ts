45c0e29175ca22738f671b34b61781b49cff0b13
txzira <txziraio@gmail.com>
2024-07-16 05:00:30 -0700

    React strict typings

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -3,6 +3,6 @@ import { revalidateTag } from "next/cache";
 
 export async function GET(request: NextRequest) {
   const tag = request.nextUrl.searchParams.get("tag");
-  revalidateTag(tag);
+  revalidateTag(tag!);
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
